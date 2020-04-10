bca1539fe4d564f32992598e0a83f126513a4ba6
alex.fan <alex.fan@okchem.com>
2020-04-10 21:46:40 +0800

    message recieved

--- index.js
+++ index.js
@@ -6,18 +6,31 @@ var http = require("http"),
 mqttServ.attachHttpServer(httpServ);
 
 // fired when a client connects
-mqttServ.on("clientConnected", function(client) {
+mqttServ.on("clientConnected", function (client) {
   console.log("Client Connected:", client.id);
 });
 
 // fired when a message is published from client
-mqttServ.on("published", function(packet, client) {
+mqttServ.on("published", function (packet, client) {
   console.log("Published", packet);
 });
 
 // fired when a client disconnects
-mqttServ.on("clientDisconnected", function(client) {
+mqttServ.on("clientDisconnected", function (client) {
   console.log("Client Disconnected:", client.id);
 });
+
+setInterval(() => {
+  var message = {
+    topic: "topic",
+    payload: "abcde", // or a Buffer
+    qos: 0, // 0, 1, or 2
+    retain: false, // or true
+  };
+
+  mqttServ.publish(message, function () {
+    console.log("done!");
+  });
+}, 2000);
 
 httpServ.listen(3000);
